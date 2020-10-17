ecc8b968d1391cc6108238a8c96fdfd54fea3015
Matthew Fitzpatrick <realmattyfitzz@Matthews-MacBook-Pro.local>
2020-10-17 15:00:39 -0700

    deploy?

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import EmployeeList from "./EmployeeList";
 import SearchBox from "./SearchBox";
-import './app.css';
+import "../src/styles/App.css"
 
 
 class App extends Component {
